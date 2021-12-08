7bf3a992d2364de5fe2f1b4d6a1efb5fd023073c
Pedro Sousa Santos <pmpss99@gmail.com>
2021-12-08 23:40:03 +0000

    Update db

---
+++ database/migrations/2021_12_08_221007_create_leagues_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateLeaguesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('leagues', function (Blueprint $table) {
+            $table->unsignedBigInteger('id')->primary();
+
+            $table->foreignId('season_id')->constrained();
+            $table->string('name');
+            $table->unsignedBigInteger('current_season_id');
+            $table->unsignedBigInteger('current_round_id');
+            $table->unsignedBigInteger('current_stage_id');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('leagues');
+    }
+}
---
+++ database/migrations/2021_12_08_233436_create_players_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePlayersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('players', function (Blueprint $table) {
+            $table->unsignedBigInteger('id')->primary();
+
+            $table->foreignId('team_id')->constrained();
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('display_name');
+            $table->string('position');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('players');
+    }
+}
